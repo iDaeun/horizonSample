e70fbf95336212f89f9c585f921476218217d966
daeun <programmerdaeun@gmail.com>
2021-04-27 17:39:37 +0900

    add horizon files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+
+declare var horizon: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'horizonSample';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    console.log('horizon', horizon);
+    if (typeof horizon.network_topology !== 'undefined') {
+      horizon.network_topology.init();
+    } else {
+      horizon.networktopologycommon.init();
+      horizon.flat_network_topology.init();
+      horizon.network_topology.init();
+    }
+  }
 }
